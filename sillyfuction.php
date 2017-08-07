29e8bc50cc4b368e4f68f2359dcbdf815f8a2b34
Ubuntu <ubuntu@192.168.197.28>
2017-08-07 23:57:08 +0000

    Attempt to decipher the first part of these Perl abbreviations

--- sillyfuction.php
+++ sillyfuction.php
@@ -12,7 +12,32 @@ class Product
 	}
 }
 
+class ProductGrouper
+{
+	public $products = [];
+
+	public function group(Array $products)
+	{
+		$brandTypes = [];
+
+		foreach ($products as $product) {
+			if (!$product instanceof Product) {
+				throw new Exception("ProductGrouper.group() requires and array of Products");
+			}
+
+			if (!isset($product->brand)) {
+				$product->brand = ''
+
+			$brandTypes[$product->brand] = [$product->type = 1];
+		}
+	}
+}
+
 $product = new Product("hello", "world");
 
 echo $product->brand;
 echo $product->type;
+
+$products[] = $product;
+
+$productGrouper = new ProductGrouper($products);
